86e964ee822f5f2a123b3b49679b92f8f9bbbcf9
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-07-02 22:44:57 +0530

    shared change 2nd

--- shared/new.js
+++ shared/new.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'shared change',
+        message: 'shared change_new',
         input: event,
       },
       null,
