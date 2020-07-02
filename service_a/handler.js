9ca6b51ccd4830fe0cf2128885ad117c2c0a6d68
sounakchatterjee00 <sounakchatterjee00@gmail.com>
2020-07-02 22:31:47 +0530

    change

--- service_a/handler.js
+++ service_a/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: '1st change',
+        message: '1st changed',
         input: event,
       },
       null,
